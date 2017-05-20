fc95888f560dae8b4f8fe77f2ae7d9492cada62c
kr5hn4 <cod3ncoff33@gmail.com>
2017-05-20 20:41:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express')
+var socket = require('socket.io')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var expressValidator = require('express-validator')
+var flash = require('connect-flash')
+var session = require('express-session')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var mongoose = require('mongoose')
+var path = require('path')
+
+
+//connect to database
+mongoose.connect('mongodb://localhost:27017/myappdatabase')
+
+// require routes
+var routes = require('./routes/index')
+var users = require('./routes/users')
+
+// initialise express app
+var app = express()
+
+// set views folder
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.set('port', 3000)
+
+// bodyParser middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+// cokkieParser
+app.use(cookieParser())
+// session middleware
+app.use(session({
+  secret: 'somerandomchars',
+  saveUninitialized: true,
+  resave: true
+}))
+// initialize passport (note: make sure to use passport.session after initializing session middleware)
+app.use(passport.initialize())
+app.use(passport.session())
+
+// connect-flash middleware
+app.use(flash())
+//set static folder
+app.use(express.static('public'))
+
+// routes
+app.use('/', routes)
+app.use('/users', users)
+
+// app.listen(3000 , function () {
+//   console.log('server started on port 3000');
+// });
+
+app.listen(app.get('port'), function () {
+  console.log('Express web server is listening on port ' + app.get('port'))
+})
---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var router = express.Router()
+
+router.get('/register', function (req, res) {
+  res.render('register')
+})
+
+router.post('/register', function (req, res) {
+  res.render('register')
+})
+
+module.exports = router
