d95c2192eb0dd666b79f689895661fb69ba00f70
kr5hn4 <cod3ncoff33@gmail.com>
2017-05-26 00:30:47 +0530

    added socket.io boilerplate

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express')
-var socket = require('socket.io')
+var app = express()
+var server = require('http').createServer(app)
+
+server.listen(3000)
+
+var io = require('socket.io').listen(server)
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var expressValidator = require('express-validator')
@@ -10,6 +15,18 @@ var LocalStrategy = require('passport-local').Strategy
 var mongoose = require('mongoose')
 var path = require('path')
 
+io.on('connection', function (socket) {
+  console.log('A user connected')
+  // Send a message after a timeout of 4seconds
+  setTimeout(function () {
+    socket.send('message sent from socket io after 4 seconds')
+  }, 4000)
+  // Whenever someone disconnects this piece of code executed
+  socket.on('disconnect', function () {
+    console.log('A user disconnected')
+  })
+})
+
 // connect to database
 mongoose.connect('mongodb://localhost:27017/myappdatabase')
 
@@ -18,7 +35,7 @@ var routes = require('./routes/index')
 var users = require('./routes/users')
 
 // initialise express app
-var app = express()
+// var app = express()
 
 // set views folder
 app.set('views', path.join(__dirname, 'views'))
@@ -54,6 +71,6 @@ app.use('/users', users)
 //   console.log('server started on port 3000');
 // });
 
-app.listen(app.get('port'), function () {
-  console.log('Express web server is listening on port ' + app.get('port'))
-})
+// app.listen(app.get('port'), function () {
+//   console.log('Express web server is listening on port ' + app.get('port'))
+// })
