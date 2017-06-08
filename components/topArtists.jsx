cfbaf56c04c653400ee77dc81c902e1793fb5636
kr5hn4 <kr5hn4@users.noreply.github.com>
2017-06-08 14:51:04 +0530

    created topArtists trendingSongs and Welcome modules

---
+++ components/topArtists.jsx
@@ -0,0 +1,18 @@
+class TopArtists extends React.Component {
+  render() {
+    return (
+        <div class="col-md-4">
+            <h2 style="background-color:grey">Top Artists</h2>
+            <ol>
+                <li>2pac - 1000</li>
+                <li>Notorious BIG - 900</li> 
+                ... 
+            </ol>
+        </div>
+    );
+  }
+}
+
+
+
+
---
+++ components/trendingSongs.jsx
@@ -0,0 +1,14 @@
+class TrendingSongs extends React.Component {
+  render() {
+    return (
+        <div class="col-md-6" style="border-right: 1px solid black">
+            <h2>Trending Songs</h2>
+            <ol>
+                <li>Hit'em Up</li>
+                <li>WHo shot Ya</li>
+                ...
+            </ol>
+        </div>
+    );
+  }
+}
