9d1006b883754458fdadfb7eeedbd9acf9077243
kr5hn4 <kr5hn4@users.noreply.github.com>
2017-06-13 22:15:33 +0530

    added passport code

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,56 @@
 var express = require('express')
 var router = express.Router()
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    var user = username
+    if (user === 'johny') {
+      var err = 'error'
+      done(err)
+    } else {
+      done(null, user)
+    }
+  }))
+
+passport.serializeUser(function (user, done) {
+  done(null, user)
+})
+
+passport.deserializeUser(function (user, done) {
+  // User.getUserById(user, function (err, user) {
+  //   done(err, user)
+  // })
+  done(null, user)
+})
+
+function ensureAuthenticated (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
+    // req.flash('error_msg','You are not logged in');
+    res.redirect('http://localhost:3000/')
+  }
+}
 
 router.get('/', function (req, res) {
   res.render('index')
 })
 
-router.get('/home', function (req, res) {
+router.get('/home', ensureAuthenticated, function (req, res) {
   res.render('home')
 })
 
-router.get('/scoreBoard', function (req, res) {
+router.get('/scoreBoard', ensureAuthenticated, function (req, res) {
   res.render('scoreBoard')
 })
 
-router.get('/:artistName', function (req, res) {
+router.get('/:artistName', ensureAuthenticated, function (req, res) {
   res.render('artist')
 })
 
-router.get('/:userName', function (req, res) {
+router.get('/:userName', ensureAuthenticated, function (req, res) {
   res.render('user')
 })
 
