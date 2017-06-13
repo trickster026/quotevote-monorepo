f1c680e7b9133d2d25aaf1410025af8098be86c4
kr5hn4 <kr5hn4@users.noreply.github.com>
2017-06-13 22:08:28 +0530

    added home, scoreboard, artist, and user  routes

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,20 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/home', function (req, res) {
+  res.render('home')
+})
+
+router.get('/scoreBoard', function (req, res) {
+  res.render('scoreBoard')
+})
+
+router.get('/:artistName', function (req, res) {
+  res.render('artist')
+})
+
+router.get('/:userName', function (req, res) {
+  res.render('user')
+})
+
 module.exports = router
