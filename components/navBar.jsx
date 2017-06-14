c89331ca0bd8eaeb7501212f4dbcade8579297ef
kr5hn4 <kr5hn4@users.noreply.github.com>
2017-06-14 16:30:30 +0530

    added artist, home, scoreBoard, user views and corrected navBar.jsx

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,30 +1,30 @@
 class NavBar extends React.Component {
             render() {
                 return (
-                    <div class="container">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapsible">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapsible">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
                             </button>
-                            <a class="navbar-brand" href="#">HHSB</a>
+                            <a className="navbar-brand" href="#">HHSB</a>
                         </div>
 
-                        <div class="navbar-collapse collapse" id="navbar-collapsible">
+                        <div className="navbar-collapse collapse" id="navbar-collapsible">
 
-                            <ul class="nav navbar-nav navbar-left">
+                            <ul className="nav navbar-nav navbar-left">
                                 <li><a href="#">ScoreBoard</a></li>
                                 <li><a href="#">Sign In</a></li>
                             </ul>
 
-                            <form class="navbar-form navbar-right ">
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control">
-                                        <span class="input-group-btn">
-                                            <button on-click="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+                            <form className="navbar-form navbar-right ">
+                                <div className="form-group">
+                                    <div className="input-group">
+                                        <input type="text" className="form-control" />
+                                        <span className="input-group-btn">
+                                            <button className="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
                                         </span>
                                     </div>
                                 </div>
