568684ebbebd233cfbe179d24fb95b4587ea4179
jcahiles <jovancahiles@gmail.com>
2018-07-01 19:45:12 +0800

    Disabled hotjar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App/App"
 import { BrowserRouter as Router } from "react-router-dom"
+// import { hotjar } from "react-hotjar"
 
 import { ApolloProvider } from "react-apollo"
 import { Provider } from "react-redux"
@@ -12,6 +13,8 @@ import client from "./config/apollo"
 
 import "react-toastify/dist/ReactToastify.css"
 import "semantic-ui-css/semantic.min.css"
+
+// hotjar.initialize(933497, 6)
 
 ReactDOM.render(
   <ApolloProvider client={client}>
