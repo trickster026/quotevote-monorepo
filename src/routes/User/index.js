2581586f0396cd64eb15568a4c9e0aab2f9e04c5
omolina <olivermolina10@gmail.com>
2018-11-04 23:36:47 +0800

    Added posted contents in user schema relationship

--- src/routes/User/index.js
+++ src/routes/User/index.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
-import { Container, Grid, Segment, Header, Divider } from "semantic-ui-react";
-import { Query } from "react-apollo";
-import { connect } from "react-redux";
-import gql from "graphql-tag";
-import VoteHistory from "../../components/VoteHistory/VoteHistory";
-import QuoteWall from "../../components/QuoteWall/QuoteWall";
-import ProfileHeader from "../../components/UserProfile/ProfileHeader";
-import UserPlaceHolder from "../../components/UserProfile/UserPlaceHolder/UserPlaceHolder";
-import "./User.css";
-import Followers from "../../components/Followers/Followers";
-import UserText from "../../components/UserText/UserText";
+import React, { Component } from "react"
+import { Container, Grid, Segment, Header, Divider } from "semantic-ui-react"
+import { Query } from "react-apollo"
+import { connect } from "react-redux"
+import gql from "graphql-tag"
+import VoteHistory from "../../components/VoteHistory/VoteHistory"
+import QuoteWall from "../../components/QuoteWall/QuoteWall"
+import ProfileHeader from "../../components/UserProfile/ProfileHeader"
+import UserPlaceHolder from "../../components/UserProfile/UserPlaceHolder/UserPlaceHolder"
+import "./User.css"
+import Followers from "../../components/Followers/Followers"
+import UserText from "../../components/UserText/UserText"
 
 const query = gql`
-  query user($userId: String!, $creatorId: String!) {
+  query user($userId: String!) {
     user(user_id: $userId) {
       _id
       avatar
@@ -33,110 +33,113 @@ const query = gql`
           profileImageUrl
         }
       }
-    }
-    contents(creatorId: $creatorId) {
-      _id
-      domain {
-        key
+      contents {
+        _id
+        domain {
+          key
+        }
+        creator {
+          name
+        }
+        title
+        text
       }
-      creator {
-        name
-      }
-      title
-      text
     }
   }
-`;
+`
 
 class User extends Component {
   state = {
     userId: ""
-  };
+  }
   static defaultProps = {
     userId: "none"
-  };
+  }
 
   render = () => {
-    const { userId } = this.props.match.params;
+    const { userId } = this.props.match.params
+    const { creatorId } = this.props
+    console.log({ userId, creatorId })
     return (
       <Query
         query={query}
         variables={{
-          userId: userId,
-          creatorId: this.props.creatorId
+          userId: userId
         }}
       >
         {({ loading, error, data }) => {
-          if (loading) return <UserPlaceHolder/>;
-          if (error) return <div>Error: {error.message}</div>;
+          if (loading) return <UserPlaceHolder />
+          if (error) return <div>Error: {error.message}</div>
 
-          const { user, contents } = data;
+          const { user } = data
+          const { contents } = user
           const contentTitles = contents.map(content => ({
             text: content.title,
             key: content.title,
             value: content._id,
             domain: content.domain.key
-          }));
+          }))
 
-          const hideUserPostedContents = this.props.userId !== user._id;
+          const hideUserPostedContents = this.props.userId !== user._id
 
           return (
             <Segment as={Container} basic>
               <div className="profile-content">
-                <ProfileHeader
-                  user={user}
-                  texts={contentTitles}
-                />
+                <ProfileHeader user={user} texts={contentTitles} />
                 <Grid>
                   <Grid.Row columns={2}>
                     <Grid.Column floated="left" width={6}>
-                      <VoteHistory
-                        history={user.history}
-                        loading={false}
-                      />
+                      <VoteHistory history={user.history} loading={false} />
                     </Grid.Column>
                     <Grid.Column floated="right" width={10}>
                       <Grid>
                         <Grid.Row>
                           <Grid.Column>
-                            <Followers followedUsers={user._followingId}/>
+                            <Followers followedUsers={user._followingId} />
                           </Grid.Column>
                         </Grid.Row>
                         <Grid.Row>
                           <Grid.Column>
-                            <QuoteWall
-                              quotes={user.quotes}
-                            />
+                            <QuoteWall quotes={user.quotes} />
                           </Grid.Column>
                         </Grid.Row>
                       </Grid>
-                      <Grid.Row className={hideUserPostedContents ? "row-hidden" : "row-visible"}>
+                      <Grid.Row
+                        className={
+                          hideUserPostedContents ? "row-hidden" : "row-visible"
+                        }
+                      >
                         <Grid.Column>
                           <Header
                             as="h1"
-                            style={{ fontSize: 14, paddingTop: 20, paddingLeft: 10 }}
-                          > Posted Contents </Header>
-                          <Divider/>
-                          <UserText texts={contentTitles}/>
-
+                            style={{
+                              fontSize: 14,
+                              paddingTop: 20,
+                              paddingLeft: 10
+                            }}
+                          >
+                            {" "}
+                            Posted Contents{" "}
+                          </Header>
+                          <Divider />
+                          <UserText texts={contentTitles} />
                         </Grid.Column>
                       </Grid.Row>
-
                     </Grid.Column>
                   </Grid.Row>
                 </Grid>
               </div>
             </Segment>
-          );
+          )
         }}
       </Query>
-    );
-  };
+    )
+  }
 }
 
 const mapStateToProps = ({ login: { user } }) => ({
   userId: user._id,
   creatorId: user.creatorId
-});
+})
 
-export default connect(mapStateToProps)(User);
+export default connect(mapStateToProps)(User)
