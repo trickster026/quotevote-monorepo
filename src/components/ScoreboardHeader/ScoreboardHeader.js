94a8f96bb8e7f6bfc734a32cf1452120e51d1d59
omolina <olivermolina10@gmail.com>
2019-03-27 20:04:42 +0800

    Filter Top Content Page By Date Range (initial UI changes)

--- src/components/ScoreboardHeader/ScoreboardHeader.js
+++ src/components/ScoreboardHeader/ScoreboardHeader.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
-import { Grid } from "semantic-ui-react"
+import { Dropdown, Grid } from "semantic-ui-react"
 import { SEARCH_CONTENT } from "../../actions/types"
+import { DateRangePicker } from "react-dates"
 
 import "./ScoreboardHeader.css"
+
+const trigger = (
+  <span>
+    <i className="fas fa-sort-amount-down fa-2x" />
+  </span>
+)
+
+const tagOptions = [
+  {
+    key: "TopContent",
+    text: "Top Content",
+    value: "Top Content"
+  },
+
+  {
+    key: "SearchKey",
+    text: "Search Key",
+    value: "Search Key"
+  },
+  {
+    key: "DateRange",
+    text: "Date Range",
+    value: "Date Range"
+  }
+]
 
 class ScoreboardHeader extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      searchTerm: ""
+      searchTerm: "",
+      selectedOption: tagOptions[0],
+      startDate: null,
+      endDate: null,
+      focusedInput: null
     }
   }
 
@@ -21,7 +51,33 @@ class ScoreboardHeader extends Component {
     this.props.filterContent(this.state.searchTerm)
   }
 
+  handleOption = (e, data) => {
+    console.log("Selected: ", { data })
+    this.setState({ selectedOption: data })
+  }
+
+  renderInput = (
+    <div className="ui icon input">
+      <input
+        placeholder="Search..."
+        type="text"
+        onChange={this.handleInputChange}
+        style={{ height: "46px", width: "285px" }}
+      />
+    </div>
+  )
+
+  handleDateRangeChange = ({ startDate, endDate }) => {
+    this.setState({
+      startDate,
+      endDate
+    })
+
+    console.log({ startDate, endDate })
+  }
+
   render = () => {
+    const { selectedOption, startDate, endDate, focusedInput } = this.state
     return (
       <Grid.Row columns={1} className="scoreboard-rankings-header-row">
         <Grid.Column width={16} className="scoreboard-rankings-header-column">
@@ -30,13 +86,42 @@ class ScoreboardHeader extends Component {
               <h3>Scoreboard Rankings</h3>
             </center>
             <div className="scoreboard-rankings-icons">
-              <i className="fas fa-sort-amount-down fa-2x" />
+              <Dropdown
+                trigger={trigger}
+                icon={null}
+                style={{ marginRight: "5px" }}
+              >
+                <Dropdown.Menu>
+                  <Dropdown.Header icon="tags" content="Filter By" />
+                  <Dropdown.Menu scrolling>
+                    {tagOptions.map(option => (
+                      <Dropdown.Item
+                        key={option.value}
+                        {...option}
+                        onClick={this.handleOption}
+                      />
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown.Menu>
+              </Dropdown>
+              {selectedOption.value !== "Date Range" ? (
+                this.renderInput
+              ) : (
+                <DateRangePicker
+                  isOutsideRange={() => false}
+                  startDate={startDate} // momentPropTypes.momentObj or null,
+                  startDateId="startDateRange" // PropTypes.string.isRequired,
+                  endDate={endDate} // momentPropTypes.momentObj or null,
+                  endDateId="endDateRange" // PropTypes.string.isRequired,
+                  onDatesChange={this.handleDateRangeChange} // PropTypes.func.isRequired,
+                  focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                  onFocusChange={focusedInput =>
+                    this.setState({ focusedInput })
+                  } // PropTypes.func.isRequired,
+                />
+              )}
               &nbsp;&nbsp;
               <i className="fas fa-search fa-2x" onClick={this.clickSearch} />
-              &nbsp;&nbsp;
-              <div className="ui icon input">
-                <input type="text" onChange={this.handleInputChange} />
-              </div>
             </div>
           </div>
         </Grid.Column>
