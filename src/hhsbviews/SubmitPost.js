049590b99f699bfbe4ce22364ebea0ac06cd0286
Clark Kozak <clarkkozak@protonmail.com>
2020-03-22 17:40:55 -0600

    [WIP] Sucessful alert message + linted
    Changed
      - filter domains (groups) for users that are allowed
    
    Added
      - Adding SweetAlert to handle modal
      -  used eslint, i.e. added trailing commas
      - withRouter to handle reidrect
    
    Todo
      - style success alert
      - create error alert

--- src/hhsbviews/SubmitPost.js
+++ src/hhsbviews/SubmitPost.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react"
-import { Link } from "react-router-dom"
+import { withRouter } from "react-router-dom"
 import { useSelector } from 'react-redux'
 
 import { 
+  Grid,
   TextField, 
   Select,
   MenuItem,
@@ -10,127 +11,190 @@ import {
   FormControl,
   Divider,
   InputBase,
-  Modal,
   Typography,
   IconButton,
-  Tooltip
+  Tooltip,
+  Radio,
+  RadioGroup,
 } from "@material-ui/core"
 
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormLabel from '@material-ui/core/FormLabel';
+
+import { makeStyles } from "@material-ui/core/styles"
+
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormLabel from '@material-ui/core/FormLabel'
 
 import AddIcon from '@material-ui/icons/Add'
-import RemoveIcon from '@material-ui/icons/Remove';
+import RemoveIcon from '@material-ui/icons/Remove'
 
 import GridItem from "material-ui/components/Grid/GridItem"
 import Card from "material-ui/components/Card/Card"
 import CardHeader from "material-ui/components/Card/CardHeader"
 import CardBody from "material-ui/components/Card/CardBody"
 import Button from "material-ui/components/CustomButtons/Button"
+import styles from "material-ui/assets/jss/material-dashboard-pro-react/views/sweetAlertStyle.js"
+
 import GridContainer from "material-ui/components/Grid/GridContainer"
+
+import SweetAlert from "react-bootstrap-sweetalert"
+import * as copy from "clipboard-copy"
+
 
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import { CREATE_DOMAIN, SUBMIT_TEXT } from 'graphql/mutations'
 import { DOMAIN_QUERY } from 'graphql/query'
 
+const useStyles = makeStyles(styles)
 
 const inputStyles = {
   color: "#E91E63",
   fontSize: "25px",
   font: "League Spartan",
-  fontWeight: "bold"
+  fontWeight: "bold",
 }
 
-export default function SubmitPost() {
+function SubmitPost({ history }) {
+  const classes = useStyles()
+  const [alert, setAlert] = React.useState(null)
   const [title, setTitle] = useState('[Enter Title]')
   const [text, setText] = useState('')
   const [domain, setDomain] = useState({domain: {
-    title: 'Default'
+    title: 'Default',
   }})
   const [subScoreboardIsOpen, setCreateSubScoreboard] = useState(false)
   const [privacy, setPrivacy] = useState('private')
-  // const [showShareableLink, setShowShareableLink] = useState(false) 
 
-  const { user } = useSelector(state => state.loginReducer);
+  const { user } = useSelector((state) => state.loginReducer)
   const { loading, error, data } = useQuery(DOMAIN_QUERY, {
-    variables: { limit: 0 }
-   })
+    variables: { limit: 0 },
+  })
 
-  const [submitText, { data: submitData }] = useMutation(SUBMIT_TEXT);
+  const [submitText, { data: submitData }] = useMutation(SUBMIT_TEXT)
   const [createDomain, { data: domainData}] = useMutation(CREATE_DOMAIN)
 
 
   const handleSubmit =  async (event) => {
     event.preventDefault()
-    let domainResult;
+    let domainResult
     try {
-    if (subScoreboardIsOpen) {
-         domainResult= await createDomain({
+      if (subScoreboardIsOpen) {
+        domainResult= await createDomain({
           variables: {
             domain: {
               userId: user.creatorId,
               title: domain,
-              url: '/' + domain.toLowerCase(),
+              url: `/${  domain.toLowerCase()}`,
               key: domain.toLowerCase(),
               privacy,
-              description: "Description for: " + domain + " group"
-            }
-          }
+              description: `Description for: ${  domain  } group`,
+            },
+          },
         })
       }
-        const domainId = domain._id ? domain._id : domainResult.data.createDomain._id
-        const submitResult = await submitText({
-          variables: {
-            content: {
-              title: title,
-              text: text,
-              creatorId: user.creatorId,
-              domainId
-            }
-          }
-        })
-        const { _id } = submitResult.data.addContent
-        const domainKey = domain.key ? domain.key : domain.toLowerCase()
-        window.alert(`Sucessfully made post. Go to https://alpha.scoreboard.vote/boards/${domainKey}/content/${_id}`)
-      } catch (err) {
-        console.log('Something went wrong', { err })
-      }
+      const domainId = domain._id ? domain._id : domainResult.data.createDomain._id
+      const submitResult = await submitText({
+        variables: {
+          content: {
+            title: title,
+            text: text,
+            creatorId: user.creatorId,
+            domainId,
+          },
+        },
+      })
+      const { _id } = submitResult.data.addContent
+      const domainKey = domain.key ? domain.key : domain.toLowerCase()
+      successAlert(domainKey, _id)
+    } catch (err) {
+      console.log({ err })
+      // errorAlert()
     }
+  }
 
-
-  const handleText = event => {
+  const handleText = (event) => {
     setText(event.target.value)
   }
 
-  const handleTitle = event => {
+  const handleTitle = (event) => {
     setTitle(event.target.value)
   }
 
-  const handleDomain = event => {
+  const handleDomain = (event) => {
     setDomain(event.target.value)
   }
 
-  const handleCreateSubScoreboard = event => {
+  const handleCreateSubScoreboard = () => {
+    setDomain('')
     setCreateSubScoreboard(!subScoreboardIsOpen)
   }
 
-  const handlePrivacy = event => {
+  const handlePrivacy = (event) => {
     setPrivacy(event.target.value)
   }
-   
+
+  const successAlert = (domainKey, id) => {
+    const shareableLink = `/boards/${domainKey}/content/${id}`
+
+    setAlert(
+      <SweetAlert
+        success
+        style={{ display: "block", top: "50%" }}
+        title="You Created a Post!"
+        onConfirm={() => history.push(shareableLink)}
+        onCancel={() => hideAlert()}
+        confirmBtnCssClass={`${classes.button} ${classes.success}`}
+        confirmBtnText="Go to Post"
+      >
+        <Typography variant="caption">Share your text to your friends and family.</Typography>
+        <Grid 
+          container
+          justify="center" 
+          style={{ marginTop: 16 }}
+        >
+          <div>
+            { shareableLink }
+          </div>
+          <Button 
+            color="primary"
+            onClick={handleCopy(process.env.REACT_APP_DOMAIN + shareableLink)}
+          >
+            Copy
+          </Button>
+        </Grid>
+      </SweetAlert>
+    )
+  }
+
+  const hideAlert = () => {
+    setAlert(null)
+  }
+
+  const handleCopy = (shareableLink) => {
+    copy(shareableLink)
+  }
+
   if (loading) {
     return 'loading'
   }
-  else if(error) {
+  
+  if(error) {
     return 'error'
-  } else {  
-    const publicDomains = data.domains.filter(domain => {
-      return domain.privacy === 'public'
-    })
-    
+  } 
+
+  const userAllowedDomains = data.domains.filter((domain) => {
+    const isUserAllowed = domain.allowedUserIds.find(
+      (id) => id === user.creatorId
+    )
     return (
+      domain.privacy === "public" ||
+        (domain.privacy === "private" &&
+          isUserAllowed)
+    )
+  })
+
+  return (
+    <>
+      {alert}
       <GridContainer spacing={1} direction="col">
         <GridItem xs={6}>
           <Card style={{ height: "800px" }}>
@@ -140,15 +204,15 @@ export default function SubmitPost() {
                   display: "flex",
                   direction: "row",
                   justifyContent: "space-between",
-                  zIndex: 0
+                  zIndex: 0,
                 }}
-                >
+              >
                 <div
                   style={{
                     display: "flex",
                     direction: "row",
                     alignContent: "center",
-                    justifyContent: "space-between"
+                    justifyContent: "space-between",
                   }}
                 >
                   <InputBase
@@ -156,6 +220,7 @@ export default function SubmitPost() {
                     onChange={handleTitle}
                     value={title}
                     name="title"
+                    required
                   />
                 </div>
                 <div
@@ -163,7 +228,7 @@ export default function SubmitPost() {
                     display: "flex",
                     direction: "row",
                     justifyContent: "flex-end",
-                    flexBasis: "100px"
+                    flexBasis: "100px",
                   }}
                 >     
                 </div>
@@ -171,93 +236,88 @@ export default function SubmitPost() {
               <Divider />
             </CardHeader>
             <CardBody>
-            <form
-             onSubmit={handleSubmit}
-            >
-            <TextField
-              id="post"
-              label="Post"
-              placeholder="Input text to submit post"
-              multiline
-              fullWidth
-              name="text"
-              required
-              onChange={handleText}
-              value={text}
-              style={{
-                maxHeight: 600,
-                overflow: 'auto'
-              }}
-            />
-            <div style={{marginTop: 24, display: 'flex', justifyContent: 'space-between', alixgnContent: 'center'}}>
-              <div>
-              <FormControl>
-                {
-                  subScoreboardIsOpen && subScoreboardIsOpen 
-                  ? 
-                  <TextField
-                    id="group"
-                    label="Group"
-                    placeholder="Create a new group"
-                    name="group"
-                    required
-                    onChange={handleDomain}
-                    value={domain.title}  
-                    style={{ width: 280 }}
-                  />
-                : 
-                <>
-                <InputLabel id="group-label" htmlFor="group">Group</InputLabel>
-
-                  <Select
-                  id="group"
-                  value={domain.title}
-                  placeholder={domain.title}
+              <form
+                onSubmit={handleSubmit}
+              >
+                <TextField
+                  id="post"
+                  label="Post"
+                  placeholder="Input text to submit post"
+                  multiline
+                  fullWidth
+                  name="text"
                   required
-                  onChange={handleDomain}
-                  style={{ width: 280 }}
-                 >
-                   <MenuItem value={domain.title}>
-                    {domain.title}
-                   </MenuItem>
-                {publicDomains.map(publicDomain => {
-                  return (
-                  <MenuItem
-                    name={publicDomain.key}
-                    value={publicDomain}
-                    key={publicDomain._id}
-                  >
-                      {publicDomain.title}
-                  </MenuItem>)
-                })}
-                </Select>
-                </>
-                }
+                  onChange={handleText}
+                  value={text}
+                  style={{
+                    maxHeight: 600,
+                    overflow: 'auto',
+                  }}
+                />
+                <div style={{marginTop: 24, display: 'flex', justifyContent: 'space-between', alixgnContent: 'center'}}>
+                  <div>
+                    <FormControl>
+                      {
+                        subScoreboardIsOpen && subScoreboardIsOpen                   ? 
+                          <TextField
+                            id="group"
+                            label="Group"
+                            placeholder="Create a new group"
+                            name="group"
+                            required
+                            onChange={handleDomain}
+                            value={domain.title}  
+                            style={{ width: 280 }}
+                          /> : 
+                          <>
+                            <InputLabel id="group-label" htmlFor="group">Group</InputLabel>
+                            <Select
+                              id="group"
+                              value={domain.title}
+                              placeholder={domain.title}
+                              required
+                              onChange={handleDomain}
+                              style={{ width: 280 }}
+                            >
+                              <MenuItem value={domain.title}>
+                                {domain.title}
+                              </MenuItem>
+                              {userAllowedDomains.map((publicDomain) => (
+                                <MenuItem
+                                  name={publicDomain.key}
+                                  value={publicDomain}
+                                  key={publicDomain._id}
+                                >
+                                  {publicDomain.title}
+                                </MenuItem>))}
+                            </Select>
+                          </>
+                      }
                 
-              </FormControl>
-                 <IconButton 
-                  style={{ marginTop: 8, marginLeft: 4}}
-                  onClick={handleCreateSubScoreboard}
-                >
-                  { subScoreboardIsOpen && subScoreboardIsOpen 
-                   ? <Tooltip title="Choose an existing group" style={{ fontSize: 18 }}><RemoveIcon/></Tooltip>
-                   : <Tooltip title="Add a new group"><AddIcon/></Tooltip>
-                  }
-                </IconButton>
-              </div>
-              <Button 
-                type="submit"
-                variant="contained"
-                size="large"
-                style={{
-                  backgroundColor: 'rgb(233, 30, 99)'
-                }}
-               >
+                    </FormControl>
+                    <IconButton 
+                      style={{ marginTop: 8, marginLeft: 4}}
+                      onClick={handleCreateSubScoreboard}
+                    >
+                      { subScoreboardIsOpen && subScoreboardIsOpen ? 
+                        <Tooltip title="Choose an existing group" style={{ fontSize: 18 }}><RemoveIcon/></Tooltip> :
+                        <Tooltip title="Add a new group"><AddIcon/></Tooltip>
+                      }
+                    </IconButton>
+                  </div>
+                  <Button 
+                    type="submit"
+                    variant="contained"
+                    size="large"
+                    style={{
+                      backgroundColor: 'rgb(233, 30, 99)',
+                    }}
+                  >
                 Submit
-              </Button>
-            </div>
-              {
-                subScoreboardIsOpen &&
+                  </Button>
+                </div>
+                {
+                  subScoreboardIsOpen &&
                 <div style={{ paddingTop  : 16}}>
                   <FormControl component="fieldset" >
                     <FormLabel component="legend">Choose Visibility</FormLabel>
@@ -267,8 +327,8 @@ export default function SubmitPost() {
                     </RadioGroup>
                   </FormControl>
                 </div>
-              }
-            </form>
+                }
+              </form>           
             </CardBody>
           </Card>
         </GridItem>
@@ -279,17 +339,17 @@ export default function SubmitPost() {
                 style={{
                   display: "flex",
                   direction: "row",
-                  justifyContent: "space-between"
+                  justifyContent: "space-between",
                 }}
-                >
+              >
                 <p
                   style={{
                     color: "#E91E63",
                     fontSize: "25px",
                     font: "League Spartan",
-                    fontWeight: "bold"
+                    fontWeight: "bold",
                   }}
-                  >
+                >
                   Pop Prediction
                 </p>
               </div>
@@ -297,6 +357,8 @@ export default function SubmitPost() {
           </Card>
         </GridItem>
       </GridContainer>
+    </>
   )
 }
-}
+
+export default withRouter(SubmitPost)
