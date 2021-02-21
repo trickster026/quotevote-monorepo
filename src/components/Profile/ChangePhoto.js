2cc737a88e78577c98b7b8f3c7e21330fb656589
Sarah Naas <sjnaas@gmail.com>
2021-02-20 18:23:32 -0800

    Avatar building2 (#119)
    * added .eslintcache to gitignore
    
    * added bugsnag to help snag those bugs
    
    * made all eslint errors into warnings
    
    * Changed eslint errors to warnings
    
    * fixed PostCard activityType default prop
    
    * fixed onBookmark warning by removing isRequired
    
    * fixed some requirement bugs for data in PostList and LoadPostList && fixed LoadingSpinner marginTop prop
    
    * fixed propTypes for width in ActivityList and ActivityCard to match with what was coming down from props
    
    * fixed eslint error for tests
    
    * fixed propType syntax for FollowInfo
    
    * fixed propType for filterState on ProfileView
    
    * fixed propType of setOffset in ProfileView and of notifications in Notification
    
    * changed height propType to any in Avatar
    
    * added key to PostCard in PostList
    
    * changed all Proptypes of avatar to allow for any proptype for avatar in /ui/ActivityCard
    
    * added a key for messages in MessageItemList
    
    * added key to fragment in BuddyItemList
    
    * fixed  added keys to props in ChangePhoto
    
    * changed large to lg in SubmitPostForm
    
    * changed options propType to array
    
    * changed key of Typography in SubmitPostSkeleton
    
    * added key to Grid on PostList
    
    * changed propType of selectAll in FilterInputs
    
    * removed hr from variant on Divider in Notifications
    
    * changed PropTypes in to array for postActions in PostActionsList
    
    * basic outline for component
    
    * tested messages as props. All Working.
    
    * direction of messages changing based on wether which user is messaging
    
    * stash base
    
    * works fine statically
    
    * fetching data from db
    
    * broke inpt up into new component
    
    * in progress
    
    * post message chat can now access the correct room
    
    * chat directions working again
    
    * formatted time stamp
    
    * separated favorite button to a different component
    
    * got my chat bubblespositioned how i want them
    
    * mobile responsive
    
    * fixed loading issue and changed the order of the messages to newst at the top
    
    * finished up some styling
    
    * got both queries working
    
    * messages appeari in feed with everything else. + fixed some styling
    
    * fixed up eslint error
    
    * changed datetime to match comments
    
    * fixed hooks warning
    
    * removed unused components
    
    * got emoji picker showing up
    
    * message reactions working
    
    * both message reactions and action reactions are working
    
    * emoji updates working on messages
    
    * finished up emoji display
    
    * fixed moment utils
    
    * some styling changes
    
    * finished emoji display styling
    
    * made post scrollable
    
    * fixed highlighting so it responds when you click on it
    
    * put like and dislike icon back for votes
    
    * chat avatars will take you to profile
    
    * fixed post height from px to vh
    
    * changed the length of the feed
    
    * changed postpage back to two scrolls
    
    * layout for Avatar creation done
    
    * updates on avatars working
    
    * fixed up some styling
    
    * color options working
    
    * bingo button works
    
    * added hatColors
    
    * somestyling touch ups
    
    * nah button working
    
    * cleaned up warnings
    
    * cleaned up eslint errors and fixed button styling
    
    * selected option brings you back to homepage

--- src/components/Profile/ChangePhoto.js
+++ src/components/Profile/ChangePhoto.js
@@ -1,35 +1,19 @@
 import React, { useState } from 'react'
-import { useMutation } from '@apollo/react-hooks'
-import { useSelector, useDispatch } from 'react-redux'
-import _ from 'lodash'
+import { useSelector } from 'react-redux'
 
 // MUI
 import { MuiThemeProvider as ThemeProvider, makeStyles } from '@material-ui/core/styles'
 import {
-  Avatar, Typography, Grid, Button,
+  Typography, Grid,
 } from '@material-ui/core'
 
 // Local
-import { updateAvatar } from 'store/user'
-import { UPDATE_USER_AVATAR } from '../../graphql/mutations'
-import AvatarPreview from '../Avatar'
-import { avatarOptions } from '../../utils/display'
+import AvatarIconButtons from './AvatarIconButtons'
+import AvatarOptionButtons from './AvatarOptionButtons'
 import theme from '../../themes/MainTheme'
-import { SET_SNACKBAR } from '../../store/ui'
-
-// Icons
-import Silhouette from '../../assets/svg/Silhouette.svg'
-import Glasses from '../../assets/svg/Glasses.svg'
-import Mouth from '../../assets/svg/Mouth.svg'
-import Shirt from '../../assets/svg/Shirt.svg'
-import Eyes from '../../assets/svg/Eyes.svg'
-import Hat from '../../assets/svg/Hat.svg'
-import Beard from '../../assets/svg/Beard.svg'
-import Eyebrow from '../../assets/svg/Eyebrow.svg'
 
 const useStyles = makeStyles({
   fullCard: {
-    height: 'calc(95vh - 90px)',
     paddingTop: 30,
   },
   heading: {
@@ -38,72 +22,8 @@ const useStyles = makeStyles({
   },
   card: {
     backgroundColor: 'rgba(255, 255, 255, .6)',
-    borderRadius: 5,
-  },
-  optionCard: {
-    borderTop: '1px solid #CAE7FF',
-    padding: '20px 30px',
-  },
-  buttonCard: {
-    padding: '0px 30px',
-  },
-  heading2: {
-    color: '#97999A',
-    fontWeight: 'bold',
-    fontSize: 16,
-    padding: '10px 0px 0px 10px',
-  },
-  discardButton: {
-    backgroundColor: '#DB6666',
-    color: 'white',
-    fontSize: 30,
-    width: 163,
-    height: 88,
-    textTransform: 'none',
-    fontWeight: 'normal',
-    '&:hover': {
-      backgroundColor: '#E75656 !important',
-    },
-  },
-  bingoButton: {
-    backgroundColor: '#7DD6AD',
-    marginRight: 30,
-    color: 'white',
-    fontSize: 30,
-    width: 163,
-    height: 88,
-    textTransform: 'none',
-    fontWeight: 'normal',
-    '&:hover': {
-      backgroundColor: '#52E39F !important',
-    },
-  },
-  avatar: {
-    margin: 50,
-    height: 200,
-    width: 200,
-    backgroundColor: '#65C9FF',
-  },
-  size: {
-    height: 80,
-    width: 80,
-    backgroundColor: '#65C9FF',
-    margin: '5px 20px 5px 15px',
-    cursor: 'pointer',
-  },
-  svgButton: {
-    borderRadius: 100,
-    height: 80,
-    width: 80,
-    margin: '0px 30px 0px 15px',
-    '&:hover': {
-      backgroundColor: 'rgba(0, 207, 110, .5) !important',
-    },
-  },
-  avatarRow: {
-    padding: 20,
     height: 205,
-    overflowY: 'scroll',
+    borderRadius: 5,
   },
 })
 
@@ -114,11 +34,10 @@ const useStyles = makeStyles({
  */
 function ChangePhoto() {
   const user = useSelector((state) => state.user.data)
-  const [updateUserAvatar] = useMutation(UPDATE_USER_AVATAR)
   const [avatarOptionsArray, setAvatarOptionsArray] = useState()
   const [updatedAvatar, setUpdatedAvatar] = useState()
-  const [selectedOptions, setSelectedOptions] = useState()
   const [colorOptions, setColorOptions] = useState()
+  const [selectedOptions, setSelectedOptions] = useState()
   //  prevent legacy image file avatars from crapping out front end
   let defaultAvatar = {}
 
@@ -128,40 +47,9 @@ function ChangePhoto() {
     defaultAvatar = user.avatar
   }
 
-  const dispatch = useDispatch()
   const classes = useStyles()
 
-  const onSubmit = async (avatar) => {
-    const newAvatar = await updateUserAvatar({ variables: { user_id: user._id, avatarQualities: avatar } })
-    await updateAvatar(dispatch, newAvatar.data.updateUserAvatar.avatar)
-    dispatch(SET_SNACKBAR({
-      type: 'danger',
-      message: 'Avatar has been updated',
-      open: true,
-    }))
-  }
-
-  const groupedAvatarOptions = _.groupBy(avatarOptions, 'name')
-
-  const {
-    topType, accessoriesType, facialHairType, clotheType, mouthType, eyebrowType, eyeType, facialHairColor, clotheColor, hairColor, hatColor, skinColor,
-  } = groupedAvatarOptions
-
-  topType.icon = Hat
-  accessoriesType.icon = Glasses
-  facialHairType.icon = Beard
-  clotheType.icon = Shirt
-  skinColor.icon = Silhouette
-  mouthType.icon = Mouth
-  eyeType.icon = Eyes
-  eyebrowType.icon = Eyebrow
-
-  const buttonOptions = []
-
-  buttonOptions.push(eyebrowType, topType, accessoriesType, facialHairType, clotheType, skinColor, mouthType, eyeType)
-
   const displayAvatarOptions = []
-  const displayColorOptions = []
 
   function handleIconClick(category) {
     const { name, options } = category
@@ -181,7 +69,7 @@ function ChangePhoto() {
           avatarCategoryDisplay.eyebrowType = options[i]
           displayAvatarOptions.push(avatarCategoryDisplay)
         }
-        setSelectedOptions(null)
+        setSelectedOptions(name)
         setColorOptions(null)
         setAvatarOptionsArray(displayAvatarOptions)
         break
@@ -191,7 +79,7 @@ function ChangePhoto() {
           avatarCategoryDisplay.eyeType = options[i]
           displayAvatarOptions.push(avatarCategoryDisplay)
         }
-        setSelectedOptions(null)
+        setSelectedOptions(name)
         setColorOptions(null)
         setAvatarOptionsArray(displayAvatarOptions)
         break
@@ -210,7 +98,7 @@ function ChangePhoto() {
           avatarCategoryDisplay.skinColor = options[i]
           displayAvatarOptions.push(avatarCategoryDisplay)
         }
-        setSelectedOptions(null)
+        setSelectedOptions(name)
         setColorOptions(null)
         setAvatarOptionsArray(displayAvatarOptions)
         break
@@ -229,7 +117,7 @@ function ChangePhoto() {
           avatarCategoryDisplay.mouthType = options[i]
           displayAvatarOptions.push(avatarCategoryDisplay)
         }
-        setSelectedOptions(null)
+        setSelectedOptions(name)
         setColorOptions(null)
         setAvatarOptionsArray(displayAvatarOptions)
         break
@@ -239,7 +127,7 @@ function ChangePhoto() {
           avatarCategoryDisplay.accessoriesType = options[i]
           displayAvatarOptions.push(avatarCategoryDisplay)
         }
-        setSelectedOptions(null)
+        setSelectedOptions(name)
         setColorOptions(null)
         setAvatarOptionsArray(displayAvatarOptions)
         break
@@ -248,127 +136,16 @@ function ChangePhoto() {
     }
   }
 
-  function handleSelectAvatarOption(option) {
-    setUpdatedAvatar(option)
-    let colors
-    switch (selectedOptions) {
-      case 'topType':
-        if (option.topType.includes('WinterHat') || option.topType === 'Hijab' || option.topType === 'Turban') {
-          colors = hatColor[0].options
-          for (let i = 0; i < colors.length; i++) {
-            const avatarCategoryDisplay = { ...option }
-            avatarCategoryDisplay.hatColor = colors[i]
-            displayColorOptions.push(avatarCategoryDisplay)
-          }
-        } else if (option.topType === 'LongHairFrida' || option.topType === 'Eyepatch' || option.topType === 'NoHair' || option.topType === 'Hat' || option.topType === 'LongHairShavedSides') {
-          const avatarCategoryDisplay = { ...option }
-          displayColorOptions.push(avatarCategoryDisplay)
-        } else {
-          colors = hairColor[0].options
-          for (let i = 0; i < colors.length; i++) {
-            const avatarCategoryDisplay = { ...option }
-            avatarCategoryDisplay.hairColor = colors[i]
-            displayColorOptions.push(avatarCategoryDisplay)
-          }
-        }
-        setColorOptions(displayColorOptions)
-        break
-      case 'facialHairType':
-        if (option.facialHairType === 'Blank') {
-          const avatarCategoryDisplay = { ...option }
-          displayColorOptions.push(avatarCategoryDisplay)
-        } else {
-          colors = facialHairColor[0].options
-          for (let i = 0; i < colors.length; i++) {
-            const avatarCategoryDisplay = { ...option }
-            avatarCategoryDisplay.facialHairColor = colors[i]
-            displayColorOptions.push(avatarCategoryDisplay)
-          }
-        }
-        setColorOptions(displayColorOptions)
-        break
-      case 'clotheType':
-        if (option.clotheType.includes('Blazer')) {
-          const avatarCategoryDisplay = { ...option }
-          displayColorOptions.push(avatarCategoryDisplay)
-        } else {
-          colors = clotheColor[0].options
-          for (let i = 0; i < colors.length; i++) {
-            const avatarCategoryDisplay = { ...option }
-            avatarCategoryDisplay.clotheColor = colors[i]
-            displayColorOptions.push(avatarCategoryDisplay)
-          }
-        }
-        setColorOptions(displayColorOptions)
-        break
-      default:
-        setColorOptions(null)
-    }
-  }
-
   return (
     <ThemeProvider theme={theme}>
-      <Grid container display="flex" direction="row" className={classes.fullCard}>
-        <Grid container item display="flex" direction="column" alignItems="center" xs={6}>
-          <Grid item>
-            <Typography className={classes.heading}>
-              Create your avatar
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Avatar className={classes.avatar}>
-              <AvatarPreview {...defaultAvatar} />
-            </Avatar>
-          </Grid>
-          <Grid item>
-            <Button
-              type="submit"
-              className={classes.bingoButton}
-              onClick={() => onSubmit(defaultAvatar)}
-            >
-              Bingo
-            </Button>
-            <Button
-              className={classes.discardButton}
-              onClick={() => setUpdatedAvatar(user.avatar)}
-            >
-              Nah
-            </Button>
-          </Grid>
+      <Grid container className={classes.fullCard} display="flex" direction="column" alignItems="center">
+        <Grid item>
+          <Typography className={classes.heading}>
+            Create your avatar
+          </Typography>
         </Grid>
-        <Grid container item display="flex" direction="column" className={classes.card} xs={6}>
-          <Grid item>
-            <Typography className={classes.heading2}>
-              choose a feature to customize
-            </Typography>
-          </Grid>
-          <Grid container display="flex" direction="column" className={classes.buttonCard}>
-            <Grid item container display="flex" direction="row" justify="space-evenly" className={classes.avatarRow}>
-              {buttonOptions.map((category) => (
-                <Button className={classes.svgButton} hover display="flex" justify="center" alignItems="center" onClick={() => handleIconClick(category[0])}>
-                  <img src={category.icon} alt={category.name} />
-                </Button>
-              ))}
-            </Grid>
-          </Grid>
-          <Grid container display="flex" direction="column" className={classes.optionCard}>
-            <Grid item container display="flex" direction="row" justify="space-evenly" className={classes.avatarRow}>
-              {avatarOptionsArray && avatarOptionsArray.map((option) => (
-                <Avatar className={classes.size} onClick={() => handleSelectAvatarOption(option)}>
-                  <AvatarPreview {...option} />
-                </Avatar>
-              ))}
-            </Grid>
-          </Grid>
-          <Grid container display="flex" direction="column" className={classes.optionCard}>
-            <Grid item container display="flex" direction="row" justify="space-evenly" className={classes.avatarRow}>
-              {colorOptions && colorOptions.map((option) => (
-                <Avatar className={classes.size} onClick={() => setUpdatedAvatar(option)}>
-                  <AvatarPreview {...option} />
-                </Avatar>
-              ))}
-            </Grid>
-          </Grid>
+        <Grid item container>
+          {!selectedOptions ? <AvatarIconButtons setSelectedOptions={setSelectedOptions} defaultAvatar={defaultAvatar} updatedAvatar={updatedAvatar} handleIconClick={handleIconClick} /> : <AvatarOptionButtons avatarOptionsArray={avatarOptionsArray} setUpdatedAvatar={setUpdatedAvatar} updatedAvatar={updatedAvatar} selectedOptions={selectedOptions} setSelectedOptions={setSelectedOptions} />}
         </Grid>
       </Grid>
     </ThemeProvider>
