0df12c4efe54de7a3f567532738dad4f7a9a15d5
Oliver Molina <olivermolina10@gmail.com>
2025-06-28 18:21:26 +0800

    chore: Add ecosystem configuration for process management with PM2

---
+++ server/app/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    apps: [
+      {
+        name: 'HHSB',
+        script: 'dist/server.js',
+        env: {
+          NODE_ENV: process.env.NODE_ENV || 'production',
+          SECRET: process.env.SECRET || '',
+          LYRICIST_TOKEN: process.env.LYRICIST_TOKEN || '',
+          DATABASE_URL: process.env.DATABASE_URL || '',
+          SMTP_HOST: process.env.SMTP_HOST || '',
+          SMTP_PORT: process.env.SMTP_PORT || '',
+          SMTP_USER: process.env.SMTP_USER || '',
+          SMTP_PASS: process.env.SMTP_PASS || '',
+          FROM_EMAIL: process.env.FROM_EMAIL || '',
+          CLIENT_URL: process.env.CLIENT_URL || '',
+          REQUEST_ACCESS_URL: process.env.REQUEST_ACCESS_URL || '',
+        },
+      },
+    ],
+  };
+  
